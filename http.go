fa44d83e67ab9aaa7b507dd34c89a7a4c0ee01e9
Davy Van Den Steen <davy.vandensteen@ametrine.be>
2020-02-18 15:15:41 +0100

    Added an Wrapper fulfilling the the Do function on http client

---
+++ http.go
@@ -0,0 +1,16 @@
+package retryablehttp
+
+import "net/http"
+
+type RetryingHTTPClient struct {
+	*Client
+}
+
+func (c *RetryingHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	wrappedReq, err := FromRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.Client.Do(wrappedReq)
+}
